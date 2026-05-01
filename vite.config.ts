ff9d5b6f7bd1b7f06c61d989c08b54e5e74d955c
skye <67734042+sky-larke@users.noreply.github.com>
2026-05-01 16:25:15 -0500

    Change App Title and Icon + Make an initial schedule page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://github.com/reactjs/react.dev/issues/8353
 export default defineConfig({
@@ -13,6 +14,11 @@ export default defineConfig({
       plugins: ['babel-plugin-react-compiler'],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
